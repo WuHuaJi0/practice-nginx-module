6a53c63f6663ed5bc0cd0af28b677218d49757e1
wuhuaji0 <wuhuaji0@gmail.com>
2019-11-12 23:17:41 +0800

    读取到配置参数

--- src/ngx_http_print_module.c
+++ src/ngx_http_print_module.c
@@ -6,6 +6,8 @@
 
 static char *ngx_http_print(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 static ngx_int_t ngx_http_print_handler(ngx_http_request_t *r);
+static void *ngx_create_loc_conf(ngx_conf_t *cf);
+static void *ngx_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
 
 static ngx_http_module_t ngx_http_print_module_ctx = {
         NULL,/* preconfiguration */
@@ -14,15 +16,36 @@ static ngx_http_module_t ngx_http_print_module_ctx = {
         NULL,/* init main configuration */
         NULL,/* create server configuration */
         NULL,/* merge server configuration */
-        NULL, /* create location configuration */
-        NULL/* merge location configuration */
+        ngx_create_loc_conf,
+        ngx_merge_loc_conf,
 };
+
+static void *ngx_create_loc_conf(ngx_conf_t *cf ){
+    ngx_http_print_loc_conf_t *conf;
+
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_print_loc_conf_t));
+    if (conf == NULL) {
+        return NGX_CONF_ERROR;
+    }
+    conf->print.len = 0;
+    conf->print.data = NULL;
+    return conf;
+}
+
+static void *ngx_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child){
+    ngx_http_print_loc_conf_t *prev = parent;
+    ngx_http_print_loc_conf_t *conf = child;
+
+    printf("prev %s\n conf %s \n", prev->print.data, conf->print.data);
+    ngx_conf_merge_str_value(conf->print, prev->print, "default value");
+    return NGX_CONF_OK;
+}
+
 
 static ngx_command_t ngx_http_print_commands[] = {
         {
                 ngx_string("print"),
-                NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF
-                | NGX_CONF_NOARGS, // 先用一个NO_ARGS, 不接收参数；
+                 NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
                 ngx_http_print,
                 NGX_HTTP_LOC_CONF_OFFSET,
                 0,
@@ -52,7 +75,9 @@ static ngx_int_t ngx_http_print_handler(ngx_http_request_t *r) {
     ngx_http_print_loc_conf_t *config;
     config = ngx_http_get_module_loc_conf(r, ngx_http_print_module);
 
-    /*设置 content-type */
+    printf("读取到的config配置；%s\n",config->print.data); //这里读取到的是null;
+
+    /*设置 content-type ,content-type 应该为 ngx_str_t 类型*/
     r->headers_out.content_type.len = sizeof("text/plain") - 1;
     r->headers_out.content_type.data = (u_char *) "text/plain";
 
@@ -61,16 +86,13 @@ static ngx_int_t ngx_http_print_handler(ngx_http_request_t *r) {
     buf = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
     chain[0].buf = buf;
     chain[0].next = NULL;
-//    buf->pos = config->print.data;
-//    buf->last = buf->pos + config->print.len;
-    buf->pos = "hello";
-    buf->last = buf->pos + 5;
+    buf->pos = config->print.data;
+    buf->last = buf->pos + config->print.len;
     buf->memory = 1;
     buf->last_buf = 1;
 
     r->headers_out.status = NGX_HTTP_OK;
-//    r->headers_out.content_length_n = config->print.len;
-    r->headers_out.content_length_n = 5;
+    r->headers_out.content_length_n = config->print.len;
 
     ngx_int_t result_send_header = ngx_http_send_header(r);
     if (result_send_header == NGX_ERROR || result_send_header > NGX_OK || r->header_only) {
@@ -85,10 +107,14 @@ static ngx_int_t ngx_http_print_handler(ngx_http_request_t *r) {
 static char *ngx_http_print(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
     // 定义一个配置块看结构，这里的 clcf 看上去像 loc 级别的，
     //但实际上可以是main 或者 src 或者 loc 级别都行, http{} server{} 内部都有一个 _core_loc_conf_t
-    ngx_http_core_loc_conf_t *clcf;
+    ngx_http_print_loc_conf_t *mycf = conf;
+    ngx_str_t *values =cf->args->elts;
+    mycf->print = values[1];
 
     // clcf 就是其所属的配置块
     // 我的理解：在全部配置中，找到插件所属的配置块（loc/server/http）
+
+    ngx_http_core_loc_conf_t *clcf;
     clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
     clcf->handler = ngx_http_print_handler;
     return NGX_CONF_OK;
